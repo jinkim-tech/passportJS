a352699fbdf4eb3d60a522973e8e70eb5ca2d37d
jinkim-tech <jhkim8268@gmail.com>
2019-12-26 17:19:47 -0800

    apply passport-session

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var helmet = require('helmet');
 app.use(helmet());
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var flash = require('connect-flash');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,15 +21,42 @@ app.use(
 	})
 );
 
+/*
+flash는 세션을 사용하여, session이후에 적어줘야 한다,
+*/
+app.use(flash());
+
 var authData = {
 	email: 'egoing777@gmail.com',
 	password: '111111',
 	nickname: 'egoing'
 };
 
+/* 
+passport 설치
+*/
 var passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy;
 
+app.use(passport.initialize()); // app 동작시 초기화된 passport를 사용하고
+app.use(passport.session()); // session을 passport의 로그인 인증 방식으로 사용한다
+
+/* 
+In order to support login sessions, 
+Passport will serialize and deserialize user instances to and from the session.
+세션을 처리하는 방법
+*/
+passport.serializeUser(function(user, done) { //user는 로그인 성공 시 들어온 user정보. 해당 정보를 session값에 초기 저장하는 것이 serizlizeUser
+	done(null, user.email) //user.email은 사용자의 식별자. done의 두번째 인자로 전해주기로 약속. 식별자 값은 session데이터에 user값으로 간다.
+});
+
+passport.deserializeUser(function(id, done) { //로그인 이후 페이지에 방문할때마다 호출 로그인한 사용자인지를 더블체크하는 코드.
+	done(null, authData); //authData는 유저 확인을 위한 데이터. DB이면 유저 데이터를 보낼 수 있게 확인하는 정보. serialize에서 저장된 user값과 비교.
+});
+
+/* 
+사용자가 로그인을 시도할때, 성공/실패를 확인하는 부분. 사용자가 로그인을 시도할때마다, 두번째 콜백함수가 실행.
+*/
 passport.use(
 	new LocalStrategy(
 		{
@@ -52,12 +80,15 @@ passport.use(
 	)
 );
 
+/* 
+passport가 사용자의 로그인을 처리하기 위한 방법. 전달된 정보를 passport가 받아서 성공이면 root로 실패면 login페이지로 보내기.
+*/
 app.post(
 	'/auth/login_process',
-	passport.authenticate('local', {
-		//로그인 방식 중 id & pw 전략
+	passport.authenticate('local', { //로그인 방식 중 id & pw 전략
 		successRedirect: '/',
-		failureRedirect: '/auth/login'
+		failureRedirect: '/auth/login',
+		failureFlash: true
 	})
 );
 
--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,19 @@ var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
 
-router.get('/login', function (request, response) {
-  var title = 'WEB - login';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
+router.get('/login', function(request, response) {
+	var fmsg = request.flash();
+	var feedback = '';
+	if (fmsg.error) {
+		feedback = fmsg.error[0];
+	}
+	var title = 'WEB - login';
+	var list = template.list(request.list);
+	var html = template.HTML(
+		title,
+		list,
+		`
+    <div>${feedback}</div>
     <form action="/auth/login_process" method="post">
       <p><input type="text" name="email" placeholder="email"></p>
       <p><input type="password" name="pwd" placeholder="password"></p>
@@ -16,14 +25,20 @@ router.get('/login', function (request, response) {
         <input type="submit" value="login">
       </p>
     </form>
-  `, '');
-  response.send(html);
+  `,
+		''
+	);
+	response.send(html);
 });
 
-router.get('/logout', function (request, response) {
-  request.session.destroy(function(err){
-    response.redirect('/');
-  });
+/* 
+로그아웃 기능
+*/
+router.get('/logout', function(request, response) {
+	request.logout();
+	request.session.save(function() {
+		response.redirect('/');
+	});
 });
 
 module.exports = router;
